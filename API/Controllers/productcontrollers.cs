031c7e8c4d66cb8fe65df44163397106fa0469ea
unknown <Dharmendrapratapsingh>
2021-11-13 11:44:26 +0530

    End of section 3

--- API/Controllers/productcontrollers.cs
+++ API/Controllers/productcontrollers.cs
@@ -5,26 +5,25 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using Core.Interfaces;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/products")]
 
-    public class ProductControllers : ControllerBase
+    public class ProductController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductControllers(StoreContext context)
-    
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
+           _repo = repo; 
         }
         
         [HttpGet]
-        public async Task< ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -32,8 +31,18 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
         
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 
 namespace Infrastructure.Data
@@ -16,5 +17,12 @@ namespace Infrastructure.Data
         SQLitePCL.Batteries.Init();
     }
         public DbSet<Product> Products { get; set; } 
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
